bf31fb469b38ee61c6ebaaaaaeb3f8f9a1de4584
Daniel <Daniel@DESKTOP-EICR6RV>
2025-01-08 11:41:10 +0100

    Added TODO list and math game class

---
+++ STUDY.MathGame/MathGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STUDY.MathGame
+{
+    internal class MathGame
+    {
+        private List<string> _history;
+
+
+
+    }
+}
--- STUDY.MathGame/Program.cs
+++ STUDY.MathGame/Program.cs
@@ -2,9 +2,27 @@
 {
     internal class Program
     {
+
+        // TO-DO
+        //1. create Math Game involving 4 basic operations
+        //2. Divisions should result in INTEGERS only (eg.: 10/2, 6/3 ....;option like 15/10 should not be presented)
+        //3. Users should be presented with Menu to choose an operation
+        //4. History of games should be presented in list + menu should contain option to list history of games (dont need to be stored in database)
+
+        // Challenges:
+        //1. Implement dificulty levels
+        //2. Add timer to track how longs games took (bonus: add average time per question)
+        //3. Add random game option which will contain all 4 basic operation
+        //4. Refactor code to follow DRY priciple
+
+        // Advanced challenge:
+        //1. Add voice recognition using Azure Language service
+        //https://www.youtube.com/watch?v=XMJS-eQ4Y48&ab_channel=TheC%23Academy
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
