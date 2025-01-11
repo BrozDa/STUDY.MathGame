ab05f225bebaabb0685cce0542093536829e934d
Daniel <Daniel@DESKTOP-EICR6RV>
2025-01-11 11:53:50 +0100

    Added documentation for display class + got id of magic numbers

--- STUDY.MathGame/DisplayClass.cs
+++ STUDY.MathGame/DisplayClass.cs
@@ -6,24 +6,37 @@ using System.Threading.Tasks;
 
 namespace STUDY.MathGame
 {
+    /// <summary>
+    /// Static class supporting all menu outputs 
+    /// </summary>
     internal static class DisplayClass
     {
+        /// <summary>
+        /// Prints header for each output
+        /// </summary>
+        /// <param name="roundNumber">Optional parameter, if not specified then header for completely new game is printed</param>
         public static void PrintHeader(int roundNumber = 0)
         {
             Console.Clear();
+            int windowWidth = Console.WindowWidth;
+            string welcomeMessage = "Welcome to Math Game";
+            int leftPadding = (windowWidth - welcomeMessage.Length)/ 2;
 
             if (roundNumber == 0) {
-                Console.WriteLine(new string('#', 88));
-                Console.WriteLine(new string(' ', 34) + "Welcome to Math Game");
-                Console.WriteLine(new string('#', 88));
+                Console.WriteLine(new string('#', windowWidth));
+                Console.WriteLine(new string(' ', leftPadding) + "Welcome to Math Game");
+                Console.WriteLine(new string('#', windowWidth));
             }
             else
             {
-                Console.WriteLine(new string('#', 88));
-                Console.WriteLine(new string(' ', 34) + "Math Game round " + roundNumber);
-                Console.WriteLine(new string('#', 88));
+                Console.WriteLine(new string('#', windowWidth));
+                Console.WriteLine(new string(' ', leftPadding) + "Math Game round " + roundNumber);
+                Console.WriteLine(new string('#', windowWidth));
             }
         }
+        /// <summary>
+        /// Prints selection menu
+        /// </summary>
         public static void PrintMenu()
         {
             PrintHeader();
@@ -37,10 +50,17 @@ namespace STUDY.MathGame
             Console.WriteLine();
             Console.WriteLine("6. Show game history");
             Console.WriteLine("7. Exit game");
+            Console.WriteLine();
             Console.WriteLine("Please enter a number representing your choice followed-up by 'Enter': ");
             Console.Write("Your choice: ");
 
         }
+        /// <summary>
+        /// Prints equation for current round
+        /// </summary>
+        /// <param name="roundNumber">number of the round</param>
+        /// <param name="numbers">array of numbers used for equation</param>
+        /// <param name="operation">opearation characted</param>
         public static void PrintGame(int roundNumber, int[] numbers, char operation)
         {
             
@@ -48,7 +68,14 @@ namespace STUDY.MathGame
             
             Console.Write($"{numbers[0]} {operation} {numbers[1]} = ");
         }
-
+        /// <summary>
+        /// Prints summary and correct result for lost round    
+        /// </summary>
+        // <param name="roundNumber">number of the round</param>
+        /// <param name="numbers">array of numbers used for equation</param>
+        /// <param name="operation">opearation characted</param>
+        /// <param name="userInput">Result entered by the user</param>
+        /// <param name="result">Actual result of the equation</param>
         public static void PrintEndGame(int roundNumber, int[] numbers, char operation, int userInput, int result)
         {
             PrintHeader(roundNumber);
@@ -59,6 +86,10 @@ namespace STUDY.MathGame
             Console.WriteLine("Press 'Enter' to return to the main menu or press anything else to exit");
 
         }
+        /// <summary>
+        /// Prints list of played games in current session
+        /// </summary>
+        /// <param name="history">List containing game history</param>
         public static void PrintHistory(List<string> history) {
             Console.Clear();
             PrintHeader();
@@ -73,8 +104,6 @@ namespace STUDY.MathGame
                     Console.WriteLine($"{index}: {line}");
                     index++;
                 }
-                
-
             }
             Console.WriteLine();
             Console.WriteLine("Press 'Enter' to return to the main menu or press anything else to exit");
